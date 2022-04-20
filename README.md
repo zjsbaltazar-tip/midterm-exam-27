bb8c9f377f9de10ee4cf7679677f9ad27b0f6754
zjsbaltazar-tip <qzjsbaltazar@tip.edu.ph>
2022-04-20 14:17:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+![TIP Quezon City](https://img.shields.io/badge/TIP-Quezon%20City-yellow)   ![CPE028](https://img.shields.io/badge/Course-CPE028-blue) 
+
+# Midterm Exam #27
+
+**Developer:** Zeus James S. Baltazar
+**Date Performed:** 04/20/2022; Wednesday
+
+```
+Honor Pledge:
+I affirm that I will not give or receive any unauthorized help on this activity, and that all work will be my own. I will never give this material to anyone without my consent.
+```
---
+++ midterm_app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+application = Flask(__name__, static_url_path="/static")
+
+# Index redirect to Log In View
+@application.route("/", methods=["GET"])
+def index():
+    return redirect(url_for("logInUser"))
+
+# Login View
+@application.route("/login_user", methods=["GET", "POST"])
+def logInUser():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        return redirect(url_for("index"))
+
+# Register View
+@application.route("/register_user", methods=["GET", "POST"])
+def registerUser():
+    if request.method == "GET":
+        return render_template("registration.html")
+    elif request.method == "POST":
+        return redirect(url_for("index"))
+
+if __name__=="__main__":
+    application.debug = True
+    application.run(port=5000)
